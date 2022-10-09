2ff6e6ca5c7bdad2f31d289ee198c825e8310985
Oksana_P <popovtseva.o.n@yandex.ru>
2022-10-09 14:42:34 +0500

    Add seminar 3 task 2

---
+++ Seminar_03_002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int GetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int a = GetValue("Введите номер четверти");
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Диапазон координат точек: x>0, y>0");
+        break;
+    case 2:
+        Console.WriteLine("Диапазон координат точек: x<0, y>0");
+        break;
+    case 3:
+        Console.WriteLine("Диапазон координат точек: x<0, y<0");
+        break;
+    case 4:
+        Console.WriteLine("Диапазон координат точек: x>0, y<0");
+        break;
+    default:
+        Console.WriteLine("Ошибка");
+        break;
+}
