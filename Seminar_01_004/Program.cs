9eb2ff3a6eb33368971d2e7ae7ab964a5ff8d5e4
Oksana_P <popovtseva.o.n@yandex.ru>
2022-10-09 13:51:35 +0500

    Add Task 4 from Seminar 1

---
+++ Seminar_01_004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Пользователь вводит трехзначное число. Вывести последнюю цифру этого числа
+
+Console.WriteLine("Введите трехзначное число:");
+string number1 = Console.ReadLine();
+int x = int.Parse(number1);
+int y = x / 10 * 10;
+int z = x - y;
+Console.Write("Последняя цифра введенного числа: ");
+Console.Write(z);
