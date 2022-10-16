55e7aaba0c07d0522d1f7257557188435a86f4d2
Oksana_P <popovtseva.o.n@yandex.ru>
2022-10-16 15:40:15 +0500

    Add task 1 from seminar 4

---
+++ Seminar_04_003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принимает число (А) и
+// выдает сумму чисел от 1 до А
+
+// Метод, проверяющий входные данные
+bool IsValid(int a)
+{
+    if (a <= 1)
+        return false;
+    return true;
+}
+
+int CalcSum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+// Программа
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine());
+bool InputValid = IsValid(A);
+if (InputValid)
+{
+    int Sum=CalcSum(A);
+    Console.WriteLine($"Сумма равна {Sum}");
+}
+else
+{
+    Console.WriteLine("Неправильные данные");
+}
