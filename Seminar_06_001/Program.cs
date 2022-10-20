eba1571466c9c662a05044b5555d55c207d89502
Oksana_P <popovtseva.o.n@yandex.ru>
2022-10-20 10:25:28 +0500

    Seminar 6. Part 1

---
+++ Seminar_06_001/Program.cs
@@ -0,0 +1,70 @@
+﻿//39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д)
+
+
+// Программа
+Console.Clear();
+int ArrLenght = Prompt("Введите длину массива");
+if (IsInputValid(ArrLenght))
+{
+    int[] myArray = CreateRandomArray(ArrLenght, 1, 10);
+    Console.WriteLine("Массив: { " + String.Join(" | ", myArray) + " }");
+
+    int[] revArray =CreateReverseArray(myArray);
+    Console.WriteLine("Новый перевёрнутый массив: [ " + String.Join(" | ", revArray) + " ]");
+
+    ReverseArray(myArray);
+    Console.WriteLine("Массив перевернули: ( " + String.Join(" | ", myArray) + " )");
+    Console.WriteLine("Перевернули обратно с помощью функции системы: { " + String.Join(" | ", myArray.Reverse()) + " }");
+}
+else
+{
+    Console.WriteLine("Неверная длина масива");
+
+}
+
+//Методы
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+
+bool IsInputValid(int N)
+{
+    if (N > 0) return true;
+    return false;
+}
+
+int[] CreateRandomArray(int nArr, int min, int max)
+{
+    int[] array = new int[nArr];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] CreateReverseArray(int[] Arr)
+{
+    int[] newArr = new int[Arr.Length];
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        newArr[i] = Arr[Arr.Length - 1 - i];
+    }
+    return newArr;
+}
+
+void ReverseArray(int[] Arr2)
+{
+    for (int i = 0; i < Arr2.Length / 2; i++)
+    {
+        int temp = Arr2[i];
+        Arr2[i] = Arr2[Arr2.Length - 1 - i];
+        Arr2[Arr2.Length - 1 - i] = temp;
+    }
+}
---
+++ Seminar_06_002/Program.cs
@@ -0,0 +1,87 @@
+﻿// 40: Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник со сторонами такой длина.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+//Программа
+Console.Clear();
+string sideA = GetValue("Введите длину первой стороны треугольника:");
+string sideB = GetValue("Введите длину второй стороны треугольника:");
+string sideC = GetValue("Введите длину третьей стороны треугольника:");
+bool IsAInt = IsNumberInt(sideA);
+bool IsBInt = IsNumberInt(sideB);
+bool IsCInt = IsNumberInt(sideC);
+
+if (IsAInt && IsBInt && IsCInt)
+{
+    int A = GetInt(sideA);
+    int B = GetInt(sideB);
+    int C = GetInt(sideC);
+    bool IsAValid = IsLenghtValid(A);
+    bool IsBValid = IsLenghtValid(B);
+    bool IsCValid = IsLenghtValid(C);
+
+    if (IsAValid && IsBValid && IsCValid)
+    {
+        bool IsTriangle = IsTriangleExcist(A, B, C);
+        if(IsTriangle)
+        {
+            Console.WriteLine($"Треугольник со сторонами {A}, {B} и {C} существует");
+        }
+        else
+        {
+            Console.WriteLine($"Треугольника со сторонами {A}, {B} и {C} НЕ существует");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Ошибка. Сторона треугольника должна быть больше нуля");
+    }
+}
+else
+{
+    Console.WriteLine("Неверные данные");
+}
+
+
+// Метод для считывания строки
+string GetValue(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return value;
+}
+
+//Метод для проверки числа int
+bool IsNumberInt(string valueB)
+{
+    int Exp2;
+    bool IsIntNum = Int32.TryParse(valueB, out Exp2);
+    if (IsIntNum)
+        return true;
+    return false;
+}
+
+//Метод для перевода числа
+int GetInt(string mess)
+{
+    int num = int.Parse(mess);
+    return num;
+}
+
+//Метод для проверки длины массива
+bool IsLenghtValid(int num)
+{
+    if (num > 0)
+        return true;
+    return false;
+}
+
+// Метод для проерки существования треугольника
+bool IsTriangleExcist(int a, int b, int c)
+{
+    if (a < (b + c) && b < (a + c) && c < (a + b))
+        return true;
+    return false;
+}
