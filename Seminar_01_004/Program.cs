16f84a43db1956a2e008f4a3fab40d177bfabaac
Oksana_P <114103389+OksanaPopovtseva@users.noreply.github.com>
2022-10-22 10:56:55 +0500

    Delete Seminar_01_004 directory

--- Seminar_01_004/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Пользователь вводит трехзначное число. Вывести последнюю цифру этого числа
-
-Console.WriteLine("Введите трехзначное число:");
-string number1 = Console.ReadLine();
-int x = int.Parse(number1);
-int y = x / 10 * 10;
-int z = x - y;
-Console.Write("Последняя цифра введенного числа: ");
-Console.Write(z);
