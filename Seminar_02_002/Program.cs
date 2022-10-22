3451dc7cc1c07c4dfe70dc2032ae62e876a3e0d7
Oksana_P <114103389+OksanaPopovtseva@users.noreply.github.com>
2022-10-22 10:57:11 +0500

    Delete Seminar_02_002 directory

--- Seminar_02_002/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Напишите программу, которая выводит целое случайное трехзначное число 
-// и удаляет вторую цифру этого числа. Решение через строку
-
-int number = new Random().Next(100,1000);
-Console.WriteLine(number);
-
-string A = number.ToString();
-Console.Write(A[0]);
-Console.Write(A[2]);
